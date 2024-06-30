f24a5024afbd08644777400a825f9bcb91e1f5ce
normalcat <normalcat.secret@gmail.com>
2024-06-29 22:02:48 -0700

    map additions

--- pluginprank/mapspawner.cs
+++ pluginprank/mapspawner.cs
@@ -57,6 +57,33 @@ namespace pluginprank
             Building exported607964 = new Building(rooms["HCZ_Testroom"].transform.position, rooms["HCZ_Testroom"].transform.rotation.eulerAngles - new Vector3(0, 90, 0), Vector3.one, "EXPORTED607964");
             exported607964.Add(new Structure(new Vector3(0.2187462f, 5.795532f, -0.2031326f), new Vector3(2.5f, 1f, 3f), new Vector3(0f, 0f, 0f), PrimitiveType.Cylinder, new Color(0f, 0f, 0f, 0f), "exported607964"));
             exported607964.SpawnBuilding();
+            Building exported550429 = new Building(rooms["HCZ_Nuke"].transform.position, rooms["HCZ_Nuke"].transform.rotation.eulerAngles - new Vector3(0, 0, 0), Vector3.one, "EXPORTED550429");
+            exported550429.Add(new Structure(new Vector3(-0.3431015f, 286.8965f, 2.256332f), new Vector3(11f, 0.15f, 12.5f), new Vector3(0f, 0f, 0f), PrimitiveType.Cylinder, new Color(1f, 1f, 1f, 1f), "exported550429"));
+            exported550429.SpawnBuilding();
+            Building exported574503 = new Building(rooms["Outside"].transform.position, rooms["Outside"].transform.rotation.eulerAngles - new Vector3(0, 0, 0), Vector3.one, "EXPORTED574503");
+            exported574503.Add(new Structure(new Vector3(-3.962499f, 1.459961f, -30.06564f), new Vector3(0.5f, 3f, 2.4f), new Vector3(0f, 0f, 0f), PrimitiveType.Cube, new Color(1f, 1f, 1f, 1f), "exported574503"));
+            exported574503.SpawnBuilding();
+            Building exported575133 = new Building(rooms["Outside"].transform.position, rooms["Outside"].transform.rotation.eulerAngles - new Vector3(0, 0, 0), Vector3.one, "EXPORTED575133");
+            exported575133.Add(new Structure(new Vector3(-3.962499f, 1.459961f, -33.46564f), new Vector3(0.5f, 3f, 2.1f), new Vector3(0f, 0f, 0f), PrimitiveType.Cube, new Color(1f, 1f, 1f, 1f), "exported575133"));
+            exported575133.SpawnBuilding();
+            Building exported576122 = new Building(rooms["Outside"].transform.position, rooms["Outside"].transform.rotation.eulerAngles - new Vector3(0, 0, 0), Vector3.one, "EXPORTED576122");
+            exported576122.Add(new Structure(new Vector3(-3.962499f, 2.459961f, -32.56565f), new Vector3(0.5f, 1f, 3.1f), new Vector3(0f, 0f, 0f), PrimitiveType.Cube, new Color(1f, 1f, 1f, 1f), "exported576122"));
+            exported576122.SpawnBuilding();
+            Building exported616634 = new Building(rooms["Outside"].transform.position, rooms["Outside"].transform.rotation.eulerAngles - new Vector3(0, 0, 0), Vector3.one, "EXPORTED616634");
+            exported616634.Add(new Structure(new Vector3(-8.632811f, 0.460083f, -34.10156f), new Vector3(9f, 1.05f, 0.5f), new Vector3(0f, 0f, 0f), PrimitiveType.Cube, new Color(1f, 1f, 1f, 1f), "exported616634"));
+            exported616634.SpawnBuilding();
+            Building exported617625 = new Building(rooms["Outside"].transform.position, rooms["Outside"].transform.rotation.eulerAngles - new Vector3(0, 0, 0), Vector3.one, "EXPORTED617625");
+            exported617625.Add(new Structure(new Vector3(-8.632811f, 1.510193f, -34.10156f), new Vector3(9f, 1.05f, 0.5f), new Vector3(0f, 0f, 0f), PrimitiveType.Cube, new Color(0f, 0f, 0f, 0f), "exported617625"));
+            exported617625.SpawnBuilding();
+            Building exported618643 = new Building(rooms["Outside"].transform.position, rooms["Outside"].transform.rotation.eulerAngles - new Vector3(0, 0, 0), Vector3.one, "EXPORTED618643");
+            exported618643.Add(new Structure(new Vector3(-8.632811f, 2.560181f, -34.10156f), new Vector3(9f, 1.05f, 0.5f), new Vector3(0f, 0f, 0f), PrimitiveType.Cube, new Color(1f, 1f, 1f, 1f), "exported618643"));
+            exported618643.SpawnBuilding();
+            Building exported619724 = new Building(rooms["Outside"].transform.position, rooms["Outside"].transform.rotation.eulerAngles - new Vector3(0, 0, 0), Vector3.one, "EXPORTED619724");
+            exported619724.Add(new Structure(new Vector3(-8.386719f, 3.197998f, -31.59375f), new Vector3(9.5f, 0.5f, 6f), new Vector3(0f, 0f, 0f), PrimitiveType.Cube, new Color(1f, 1f, 1f, 1f), "exported619724"));
+            exported619724.SpawnBuilding();
+            Building exported620761 = new Building(rooms["Outside"].transform.position, rooms["Outside"].transform.rotation.eulerAngles - new Vector3(0, 0, 0), Vector3.one, "EXPORTED620761");
+            exported620761.Add(new Structure(new Vector3(-8.609375f, 0.9099731f, -33.40234f), new Vector3(9f, 0.09999996f, 1f), new Vector3(0f, 0f, 0f), PrimitiveType.Cube, new Color(1f, 1f, 1f, 1f), "exported620761"));
+            exported620761.SpawnBuilding();
             Log.Info("completed building map");
         }
     }
